3d3c46d6ef50398de7173bc0882576c0ae54b306
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-06-06 22:37:18 +0200

    feat: add authentication mechanism
    It's now possible to authenticate against the
    Knockout City Server.

---
+++ src/api/auth.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import type { KOCServerUrl } from '../types/connection';
+
+type User = {
+  id: {
+    velan: number;
+  };
+  auth_provider: 'dev';
+  username: String;
+  publisher_username: String;
+  publisher_username_code: Number;
+  publisher_id: { invalid: 0 | 1 };
+  persona_id: { privsrv: 0 };
+};
+
+type AuthenticateResponse = {
+  user: User;
+  token: String;
+  country: 'US';
+};
+
+/**
+ * Authenticate against the Server.
+ *
+ * **Note**: Currently its not possible to authenticate against servers
+ * that required a secret.
+ *
+ * @param address The address at which the server is reachable
+ * @param username The username to authenticate with
+ *
+ * @returns
+ */
+export async function authenticate(address: KOCServerUrl, username: String): Promise<AuthenticateResponse> {
+  const systemGuid = crypto.randomUUID();
+  const bootSessionGuid = crypto.randomUUID();
+
+  return axios
+    .post<AuthenticateResponse>(`${address}/api/auth`, {
+      credentials: {
+        username: username,
+        platform: 'win64',
+        pid: 0,
+        system_guid: systemGuid,
+        version: 269701,
+        build: 'final',
+        boot_session_guid: bootSessionGuid,
+        is_using_epic_launcher: false,
+      },
+      auth_provider: 'dev',
+    })
+    .then((response) => response.data);
+}
---
+++ src/types/connection.ts
@@ -0,0 +1,2 @@
+export type IPAddress = `${number}.${number}.${number}.${number}`
+export type KOCServerUrl = `http://${string}:${number}` | `https://${string}:${number}`;
