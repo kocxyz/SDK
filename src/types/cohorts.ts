1d1e130ea86d3e4d5abedab680e64ad53d8f544c
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-06-09 22:24:13 +0200

    refactor: Extract `KOCCohorts`

---
+++ src/types/cohorts.ts
@@ -0,0 +1,13 @@
+export type KOCCohorts = {
+  "upsell_freq_behavior": "control_group",
+  "no_new_player_matchmaking_behavior": "no_new_player_matchmaking",
+  "linear_ftue_behavior": "control_group",
+  "breadcrumbs_no_ftue_behavior": "control_group",
+  "breadcrumbs_ftue_behavior": "control_group",
+  "daily_hideout_bonus_behavior": "control_group",
+  "new_news_screen_behavior": "control_group",
+  "extended_new_player_bots": "control_group",
+  "ps4_sim_step_slow_behavior": "control_group",
+  "xb1_sim_step_slow_behavior": "control_group",
+  "faster_new_player_bot_matchmaking": "control_group"
+}
--- src/types/events/welcome.ts
+++ src/types/events/welcome.ts
@@ -1,3 +1,4 @@
+import { KOCCohorts } from "../cohorts"
 import { KOCServerPlaylist } from "../playlist"
 import { KOCServerRegion } from "../region"
 import { KOCServerSwitches } from "../switches"
@@ -30,19 +31,7 @@ export type KOCWelcomeServerEvent = KOCEvent & {
    * The cohorts of the server
    */
   cohorts: {
-    [key: `${number}`]: {
-      "upsell_freq_behavior": "control_group",
-      "no_new_player_matchmaking_behavior": "no_new_player_matchmaking",
-      "linear_ftue_behavior": "control_group",
-      "breadcrumbs_no_ftue_behavior": "control_group",
-      "breadcrumbs_ftue_behavior": "control_group",
-      "daily_hideout_bonus_behavior": "control_group",
-      "new_news_screen_behavior": "control_group",
-      "extended_new_player_bots": "control_group",
-      "ps4_sim_step_slow_behavior": "control_group",
-      "xb1_sim_step_slow_behavior": "control_group",
-      "faster_new_player_bot_matchmaking": "control_group"
-    }
+    [key: `${number}`]: KOCCohorts
   }
   /**
    * The regions of the server
