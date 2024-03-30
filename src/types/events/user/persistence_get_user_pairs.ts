bd34c26061528a7a8049eb7ab743d5d742df703d
Tandashi <18377875+Tandashi@users.noreply.github.com>
2024-03-30 17:52:07 +0100

    fix: Typedef for `_persistence_get_user_pairs` and `_persistence_set_user_pairs`

--- src/types/events/user/persistence_get_user_pairs.ts
+++ src/types/events/user/persistence_get_user_pairs.ts
@@ -1,5 +1,4 @@
-import { KOCGroupVisibility } from '../../group';
-import { KOCUserId } from '../../user';
+import { KOCUserId, KOCUserPairs } from '../../user';
 import { KOCEvent } from '../event';
 
 export type KOCPersistenceGetUserPairsServerEvent = KOCEvent & {
@@ -10,22 +9,5 @@ export type KOCPersistenceGetUserPairsServerEvent = KOCEvent & {
 
 type UsersEntry = {
   user_id: KOCUserId;
-  pairs: {
-    equipment_data_initialized: `${number}`;
-    last_seen_event_day: `${number}`;
-    last_seen_event_week: `${number}`;
-    hideout_intro_played: `${number}`;
-    last_participated_day: `${number}`;
-    last_participated_season_day: `${number}`;
-    'season.0.last_rewarded_pass_level': `${number}`;
-    region_override: '';
-    tutorial_1_complete: `${number}`;
-    'tutorial_1_complete.or': `${number}`;
-    'tutorial_2_complete.or': `${number}`;
-    'tutorial_3_complete.or': `${number}`;
-    'tutorial_4_complete.or': `${number}`;
-    'tutorial_5_complete.or': `${number}`;
-    selected_character_index: `${0 | 1 | 2 | 3 | 4 | 5}`;
-    group_visibility?: KOCGroupVisibility;
-  };
+  pairs: Partial<KOCUserPairs>;
 };
--- src/types/user.ts
+++ src/types/user.ts
@@ -9,6 +9,20 @@ export type KOCPersonaId = {
 };
 
 export type KOCUserPairs = {
+  equipment_data_initialized: `${number}`;
+  last_seen_event_day: `${number}`;
+  last_seen_event_week: `${number}`;
+  hideout_intro_played: `${number}`;
+  last_participated_day: `${number}`;
+  last_participated_season_day: `${number}`;
+  'season.0.last_rewarded_pass_level': `${number}`;
+  region_override: '';
+  tutorial_1_complete: `${number}`;
+  'tutorial_1_complete.or': `${number}`;
+  'tutorial_2_complete.or': `${number}`;
+  'tutorial_3_complete.or': `${number}`;
+  'tutorial_4_complete.or': `${number}`;
+  'tutorial_5_complete.or': `${number}`;
+  selected_character_index: `${0 | 1 | 2 | 3 | 4 | 5}`;
   group_visibility: KOCGroupVisibility;
-  selected_character_index: 0 | 1 | 2 | 3 | 4 | 5;
 };
