5593fd27747891a05f1cf5b33bcdf15e9b12ceea
Tandashi <18377875+Tandashi@users.noreply.github.com>
2024-03-30 12:33:18 +0100

    fix: Typedef `update` for `_friend_requests_get`

--- src/types/events/friends/friend_requests_get.ts
+++ src/types/events/friends/friend_requests_get.ts
@@ -1,8 +1,23 @@
+import { KOCUserId } from '@/types';
 import { KOCEvent } from '../event';
+
+type KOCFriendRequestEntry = {
+  user_id: KOCUserId;
+  first_party: [];
+  username: string;
+  publisher_username: string;
+  publisher_username_code: number;
+  username_visible: boolean;
+  sender_persona_kind: 0;
+  last_authenticated_platform: number;
+  persona_id: {
+    invalid: 0;
+  };
+};
 
 export type KOCFriendRequestsGetServerEvent = KOCEvent & {
   type: '_friend_requests_get';
   full_update: boolean;
   full_list: boolean;
-  update: [];
+  update: KOCFriendRequestEntry[];
 };
