5192ae0c7b83d8f4b2bd8e0040b1a0b1f7424e9c
Tandashi <18377875+Tandashi@users.noreply.github.com>
2024-04-05 22:42:23 +0200

    feat: Add Typedef for `CommercePurchaseResult`

--- src/types/events/commerce/commerce_purchase_result.ts
+++ src/types/events/commerce/commerce_purchase_result.ts
@@ -1,19 +1,26 @@
 import { KOCEvent } from '@/types/events/event';
 import { KOCUserId } from '@/types/id';
 
+export type CommercePurchaseResult =
+  /**
+   * Success
+   */
+  | 0
+  /**
+   * Not enough balance
+   */
+  | 1
+  /**
+   * Already owned
+   */
+  | 2;
+
 /**
  * An event that is sent by the server when a
  * `_commerce_purchase` event is sent by the client.
  */
 export type KOCCommercePurchaseResultServerEvent = KOCEvent & {
   type: '_commerce_purchase_result';
-
-  purchase_result: // Successful
-  | 0
-    // Not enough currency
-    | 1
-    // Already owned
-    | 2;
-
+  purchase_result: CommercePurchaseResult;
   user_id: KOCUserId;
 };
