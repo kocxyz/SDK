a38c7ce7f68822bbc77f825296e87c480af6648d
Tandashi <18377875+Tandashi@users.noreply.github.com>
2024-04-17 23:18:16 +0200

    refactor: Update Typedef for `PlaylistMatchFlow`

--- src/types/playlist.ts
+++ src/types/playlist.ts
@@ -1,15 +1,22 @@
 import { UUID5Seg } from '@/types/id';
 import { KOCUserSkill } from '@/types/user';
 
+export const KOC_PLAYLIST_MATCH_FLOW_INVALID = -1;
+export const KOC_PLAYLIST_MATCH_FLOW_PRIVATE_MATCH = 0;
+export const KOC_PLAYLIST_MATCH_FLOW_TRAINING = 1;
+export const KOC_PLAYLIST_MATCH_FLOW_HIDEOUT = 2;
+export const KOC_PLAYLIST_MATCH_FLOW_QUICKPLAY = 3;
+export const KOC_PLAYLIST_MATCH_FLOW_RANKED = 4;
+export const KOC_PLAYLIST_MATCH_FLOW_DROIDS = 5;
+
 export type KOCServerPlaylistMatchFlow =
-  // Maybe Private Match
-  | 0
-  // Training
-  | 1
-  // Hideout
-  | 2
-  | 3
-  | 4;
+  | typeof KOC_PLAYLIST_MATCH_FLOW_INVALID
+  | typeof KOC_PLAYLIST_MATCH_FLOW_PRIVATE_MATCH
+  | typeof KOC_PLAYLIST_MATCH_FLOW_TRAINING
+  | typeof KOC_PLAYLIST_MATCH_FLOW_HIDEOUT
+  | typeof KOC_PLAYLIST_MATCH_FLOW_QUICKPLAY
+  | typeof KOC_PLAYLIST_MATCH_FLOW_RANKED
+  | typeof KOC_PLAYLIST_MATCH_FLOW_DROIDS;
 
 export type KOCGameServerPlaylist = {
   playlist_guid: UUID5Seg;
