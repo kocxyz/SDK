c09f546d0acdbdbbd8f836190013d270736a14d0
Tandashi <18377875+Tandashi@users.noreply.github.com>
2024-04-28 16:14:48 +0200

    fix: typedef for `KOCContractProgressGetServerEvent`

--- src/types/events/contract/contract_progress_get.ts
+++ src/types/events/contract/contract_progress_get.ts
@@ -5,22 +5,22 @@ import { Platform } from '@/types/platform';
 export type KOCContractProgressGetServerEvent = KOCEvent & {
   type: '_get_contract_progress';
   contracts: KOCContract[];
-};
-
-type KOCContract = {
-  id: number;
-  progress: {
-    [key: UUID4Seg]: KOCContractProgress;
-  };
   page_index: number;
   is_last_page: boolean;
   progress_written: boolean;
 };
-export type KOCContractState = 
+
+export type KOCContract = {
+  id: number;
+  progress: {
+    [key: UUID4Seg]: KOCContractProgress;
+  };
+};
+export type KOCContractState =
   /** In Progress */
-  | 1 
+  | 1
   /** Completed */
-  | 2 
+  | 2
   /** Rewards awarded */
-  | 3
-type KOCContractProgress = [state: KOCContractState, value: number, stage: number, platform: Platform];
+  | 3;
+export type KOCContractProgress = [state: KOCContractState, value: number, stage: number, platform: Platform];
