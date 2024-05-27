4684d0a3a799cb45d84c98a71fa43e4adb7c371b
Tandashi <18377875+Tandashi@users.noreply.github.com>
2024-05-27 23:18:57 +0200

    feat: update type definition for `_matchmaking_user_update_skill`

--- src/types/events/matchmaking/matchmaking_user_update_skill.ts
+++ src/types/events/matchmaking/matchmaking_user_update_skill.ts
@@ -1,9 +1,22 @@
-import { KOCEvent, UUID5Seg, KOCServerPlaylistMatchFlow } from '@/types';
+import { KOCEvent, UUID5Seg, KOCServerPlaylistMatchFlow, KOCUserId } from '@/types';
 
 export type KOCMatchmakingUserUpdateSkillGameServerEvent = KOCEvent & {
   type: '_matchmaking_user_update_skill';
   playlist_guid: UUID5Seg;
   match_flow: KOCServerPlaylistMatchFlow;
   season: number;
-  players: unknown[];
+  players: {
+    user_id: KOCUserId;
+    updated_mmr: number;
+    updated_skill_rating: number;
+    updated_volatility: number;
+    updated_tier: number;
+    updated_division: number;
+    updated_division_progress: number;
+    updated_win_streak: number;
+    updated_total_games_played: number;
+    updated_wins: number;
+    updated_mvps: number;
+    last_match_loss_forgiveness: number;
+  }[];
 };
